265a1b27581e9b74add115123fce195664c37429
Seyed <worldsnas@gmail.com>
2018-10-05 13:44:07 +0330

    added some modifications to indicator

--- app/src/main/java/mosleh/saeed/customview/activities/MainActivity.kt
+++ app/src/main/java/mosleh/saeed/customview/activities/MainActivity.kt
@@ -2,45 +2,30 @@ package mosleh.saeed.customview.activities
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import mosleh.saeed.customview.R
 import mosleh.saeed.customview.ViewModels.MainActivityViewModel
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private  val viewModel: MainActivityViewModel by lazy { ViewModelProviders.of(this).get(MainActivityViewModel::class.java) }
 
-
+val rand = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setContentView(R.layout.content_main)
 
-//        fab.setOnClickListener { view ->
+
+        fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                    .setAction("Action", null).show()
-//        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+            indicator.setSelectedPage(rand.nextInt(5))
+//            indicator.setPageAnimated(rand.nextInt(5))
         }
     }
+
+
 }
--- app/src/main/java/mosleh/saeed/widget/indicator/Indicator.kt
+++ app/src/main/java/mosleh/saeed/widget/indicator/Indicator.kt
@@ -1,37 +1,57 @@
 package mosleh.saeed.widget.indicator
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.support.v4.view.ViewPager
 import android.util.AttributeSet
-import android.widget.ImageView
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
 
-class Indicator : ImageView {
+class Indicator(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
-    private val duration = 1500 // ms
-    var viewPager: ViewPager? =null
-    set(value) {
-        value!!.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
-            override fun onPageScrollStateChanged(p0: Int) {
-
-            }
-
-            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
-
-            }
-
-            override fun onPageSelected(p0: Int) {
-            }
-
-        } )
+    init {
 
     }
+
+    private val duration = 1500 // ms
+    private var selectedPosition = 0
+    private var totalCount = 5
+
+    private var verticalPadding = 20
+    private var horizontalPadding = 20
+
+    private var horizontalDrawArea: Int = 0
+    private var verticalDrawArea: Int = 0
+
+    private var circleDiameter: Int = 0
+    private var circleDefaultRadius: Int = 0
+    private var circleAnimatingRadius: Int = 0
+
+    private var lastSelectedPage: Int = 0
+
+    private var selectedPage: Int = 0
+
+
+    //    var viewPager: ViewPager? =null
+//    set(value) {
+//        value!!.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+//            override fun onPageScrollStateChanged(p0: Int) {
+//
+//            }
+//
+//            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+//
+//            }
+//
+//            override fun onPageSelected(p0: Int) {
+//            }
+//
+//        } )
+//
+//    }
     private val paintStroke = Paint().apply {
         color = Color.BLACK
         isAntiAlias = true
@@ -39,23 +59,10 @@ class Indicator : ImageView {
         strokeWidth = 4f
     }
     private val paintFill = Paint().apply {
-        color = Color.BLACK
+        color = Color.GREEN
         isAntiAlias = true
         style = Paint.Style.FILL
     }
-
-    constructor(context: Context) : super(context) {
-        init()
-    }
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init()
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
-        init()
-    }
-
 
     private fun init() {
 
@@ -67,9 +74,69 @@ class Indicator : ImageView {
 //                }
     }
 
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        horizontalDrawArea = width - horizontalPadding
+        verticalDrawArea = height - verticalPadding
+        val totalCircleSpace = totalCount + (totalCount - 1)
+        circleDiameter = horizontalDrawArea / totalCircleSpace
+        circleDefaultRadius = circleDiameter / 2
+        circleAnimatingRadius = circleDefaultRadius
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        if (totalCount > 0) {
+
+            for (i in 0 until totalCount) {
+                val lengthOfPreCircles = circleDiameter * i
+                val lengthOfPreLines = circleDiameter * i
+                val cx = lengthOfPreCircles + lengthOfPreLines + circleAnimatingRadius + (horizontalPadding / 2)
+
+                var radius = circleDefaultRadius
+                var paint = paintStroke
+
+                if (i == selectedPage) {
+                    paint = paintFill
+                    radius = circleAnimatingRadius
+                }
+
+                canvas.drawCircle(cx.toFloat(),
+                        height / 2F,
+                        radius.toFloat(),
+                        paint)
+//                if (i < totalCount - 1) {
+//                    val startX = cx + circleDefaultRadius.toFloat()
+//                    canvas.drawLine(startX, (height / 2F) - 4, startX + circleDiameter, (height / 2F) - 4, paintStroke)
+//                }
+            }
+        }
+
     }
 
+    public fun setSelectedPage(selected: Int) {
+        lastSelectedPage = selectedPage
+        selectedPage = selected
+        val animator = ValueAnimator.ofInt(0, circleDefaultRadius)
+        animator.duration = 200
+        animator.interpolator = LinearInterpolator()
+        animator.addUpdateListener { animation ->
+            circleAnimatingRadius = animation.animatedValue as Int
+            invalidate()
+        }
+        animator.start()
+        invalidate()
+    }
+
+    public fun setPageAnimated(selected: Int) {
+        val animator = ValueAnimator.ofInt(0, circleDefaultRadius)
+        animator.duration = 1000
+        animator.interpolator = LinearInterpolator()
+        animator.addUpdateListener { animation ->
+            circleAnimatingRadius = animation.animatedValue as Int
+            invalidate()
+        }
+        animator.start()
+    }
 }
